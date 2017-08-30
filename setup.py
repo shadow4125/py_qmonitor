b3105bc95dbb99412e9c4a2ce836bd9cc1093094
张悦shadow <shadow.zhang@qunar.com>
2017-08-30 16:03:17 +0800

    add require

--- setup.py
+++ setup.py
@@ -4,13 +4,14 @@ from setuptools import setup
 
 setup(
     name='qmonitor_client',
-    version='0.0.4',
+    version='0.0.5',
     description='Python client for qmonitor, based on Prometheus Python Client',
     url='https://github.com/shadow4125/py_qmonitor',
     author='shadow.zhang',
     author_email='shadowyue4125@gmail.com',
     packages=['qmonitor_client', 'qmonitor_client.bridge', 'qmonitor_client.twisted'],
     extras_require={
-        'qtdigest': ['qtdigest']
-    }
+        'qtdigest': ['qtdigest'],
+    },
+    install_requires=['qtdigest']
 )
